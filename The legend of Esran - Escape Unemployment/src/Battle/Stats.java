3b6ce9307e3b471d3dcff13549d542a69858275a
dimonds-code <dimonds90dimonds@gmail.com>
2025-10-25 12:55:30 +0100

    Adding boss battle files

---
+++ The legend of Esran - Escape Unemployment/src/Battle/Stats.java
@@ -0,0 +1,10 @@
+package Battle;
+
+public class Stats {
+    public int hp, power, guard, speed;
+    public Stats(int hp, int power, int guard, int speed) {
+        this.hp = hp; this.power = power;
+        this.guard = guard; this.speed = speed;
+    }
+    public Stats copy() {return new Stats(hp, power, guard, speed);}
+}
---
+++ The legend of Esran - Escape Unemployment/src/Battle/Status.java
@@ -0,0 +1,7 @@
+package Battle;
+
+public class Status {
+    // THis will be the status of the character or opponent
+    // For example if they are OK, Ignited, Rooted, or Shocked
+
+}
